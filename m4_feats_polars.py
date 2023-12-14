6ee354ea869c18ab1ea17a477edb8fbf9b784242
root <root@DMI-5CG2243C16>
2023-12-14 00:48:56 +0000

    events rate of change

--- m4_feats_polars.py
+++ m4_feats_polars.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import polars as pl
+import re
+
 from sklearn.feature_extraction.text import CountVectorizer
 from m4_feats_functions import getEssays
 
@@ -27,7 +29,6 @@ def q3(x):
 def countvectorize_one_one(train_logs, test_logs):
 
     data = []
-
     for logs in [train_logs, test_logs]:
 
         ids = logs.id.unique()
@@ -52,7 +53,8 @@ def down_time_padding(train_logs, test_logs, time_agg):
         logs_binned = logs_binned.with_columns((pl.col('down_time') / 1000).alias('down_time_sec'))
         logs_binned = logs_binned.with_columns(((pl.col('down_time_sec') // time_agg) * time_agg).alias('time_bin'))
 
-        grp_binned = logs_binned.group_by(['id', 'time_bin']).agg(pl.max('word_count'))
+        grp_binned = logs_binned.group_by(['id', 'time_bin']).agg(pl.max('word_count'),
+                                                                  pl.count('event_id'))
         grp_binned = grp_binned.with_columns(pl.col('time_bin').cast(pl.Int64))
         grp_binned = grp_binned.sort([pl.col('id'), pl.col('time_bin')])
 
@@ -82,10 +84,10 @@ def down_time_padding(train_logs, test_logs, time_agg):
         pad_df = pl.concat(padding_dataframes).lazy()
         grp_df = pad_df.join(grp_binned.lazy(), on=['id', 'time_bin'], how='left')
         grp_df = grp_df.sort([pl.col('id'), pl.col('time_bin')])
-        grp_df = grp_df.with_columns(pl.col('word_count').fill_null(strategy="forward").over('id'))
-        data.append(grp_df.collect())
+        grp_df = grp_df.with_columns(pl.col(['word_count','event_id']).fill_null(strategy="forward").over('id'))
+        data.append(grp_df)
 
-    return data[0].lazy(), data[1].lazy()
+    return data[0], data[1]
 
 # POLARS
 def rate_of_change_feats(train_logs, test_logs):
@@ -105,6 +107,7 @@ def rate_of_change_feats(train_logs, test_logs):
 
         # Aggregating
         stats = logs.group_by('id').agg([
+            pl.col('rate_of_change').filter(pl.col('rate_of_change') == 0).count().alias('roc_zro_count'),
             pl.col('rate_of_change').count().alias('roc_count'),
             pl.col('rate_of_change').mean().alias('roc_mean'),
             pl.col('rate_of_change').std().alias('roc_std'),
@@ -116,6 +119,39 @@ def rate_of_change_feats(train_logs, test_logs):
             pl.col('rate_of_change').kurtosis().alias('roc_kurt'),
             pl.col('rate_of_change').skew().alias('roc_skew'),
         ])
+
+        feats.append(stats)
+    return feats[0], feats[1]
+
+# POLARS
+def rate_of_change_events(train_logs, test_logs):
+
+    feats = []
+    for data in [train_logs, test_logs]:
+
+        logs = data.clone()
+        logs = logs.sort('id')
+        logs = logs.with_columns([
+            pl.col('event_id').diff().over('id').alias('event_id_diff'),
+            pl.col('time_bin').diff().over('id').alias('time_bin_diff')
+        ]).fill_nan(0)
+
+        logs = logs.with_columns(
+            (pl.col('event_id_diff') / pl.col('time_bin_diff')).fill_nan(0).alias('rate_of_change')).fill_nan(0)
+
+        # Aggregating
+        stats = logs.group_by('id').agg(
+            eid_roc_count_zr = pl.col('rate_of_change').filter(pl.col('rate_of_change') == 0).count(),
+            eid_roc_count = pl.col('rate_of_change').count(),
+            eid_roc_mean = pl.col('rate_of_change').mean(),
+            eid_roc_std = pl.col('rate_of_change').std(),
+            eid_roc_max = pl.col('rate_of_change').max(),
+            eid_roc_q1 = pl.col('rate_of_change').quantile(0.25),
+            eid_roc_median = pl.col('rate_of_change').median(),
+            eid_roc_q3 = pl.col('rate_of_change').quantile(0.75),
+            eid_roc_kurt = pl.col('rate_of_change').kurtosis(),
+            eid_roc_skew = pl.col('rate_of_change').skew(),
+        )
 
         feats.append(stats)
     return feats[0], feats[1]
@@ -142,3 +178,111 @@ def wc_acceleration_feats(train_logs, test_logs, time_agg):
         feats.append(stats)
 
     return feats[0], feats[1]
+
+def q1(x):
+    return x.quantile(0.25)
+def q3(x):
+    return x.quantile(0.75)
+
+AGGREGATIONS = ['count', 'mean', 'min', 'max', 'first', 'last', q1, 'median', q3, 'sum']
+
+def reconstruct_essay(currTextInput):
+    essayText = ""
+    for Input in currTextInput.values:
+        if Input[0] == 'Replace':
+            replaceTxt = Input[2].split(' => ')
+            essayText = essayText[:Input[1] - len(replaceTxt[1])] + replaceTxt[1] + essayText[Input[1] - len(replaceTxt[1]) + len(replaceTxt[0]):]
+            continue
+        if Input[0] == 'Paste':
+            essayText = essayText[:Input[1] - len(Input[2])] + Input[2] + essayText[Input[1] - len(Input[2]):]
+            continue
+        if Input[0] == 'Remove/Cut':
+            essayText = essayText[:Input[1]] + essayText[Input[1] + len(Input[2]):]
+            continue
+        if "M" in Input[0]:
+            croppedTxt = Input[0][10:]
+            splitTxt = croppedTxt.split(' To ')
+            valueArr = [item.split(', ') for item in splitTxt]
+            moveData = (int(valueArr[0][0][1:]), int(valueArr[0][1][:-1]), int(valueArr[1][0][1:]), int(valueArr[1][1][:-1]))
+            if moveData[0] != moveData[2]:
+                if moveData[0] < moveData[2]:
+                    essayText = essayText[:moveData[0]] + essayText[moveData[1]:moveData[3]] + essayText[moveData[0]:moveData[1]] + essayText[moveData[3]:]
+                else:
+                    essayText = essayText[:moveData[2]] + essayText[moveData[0]:moveData[1]] + essayText[moveData[2]:moveData[0]] + essayText[moveData[1]:]
+            continue
+        essayText = essayText[:Input[1] - len(Input[2])] + Input[2] + essayText[Input[1] - len(Input[2]):]
+    return essayText
+
+
+def get_essay_df(df):
+    df       = df[df.activity != 'Nonproduction']
+    temp     = df.groupby('id').apply(lambda x: reconstruct_essay(x[['activity', 'cursor_position', 'text_change']]))
+    essay_df = pd.DataFrame({'id': df['id'].unique().tolist()})
+    essay_df = essay_df.merge(temp.rename('essay'), on='id')
+    return essay_df
+
+
+def word_feats(df):
+    essay_df = df
+    df['word'] = df['essay'].apply(lambda x: re.split(' |\\n|\\.|\\?|\\!',x))
+    df = df.explode('word')
+    df['word_len'] = df['word'].apply(lambda x: len(x))
+    df = df[df['word_len'] != 0]
+    word_agg_df = df[['id','word_len']].groupby(['id']).agg(AGGREGATIONS)
+    word_agg_df.columns = ['_'.join(x) for x in word_agg_df.columns]
+    word_agg_df['id'] = word_agg_df.index
+    word_agg_df = word_agg_df.reset_index(drop=True)
+    return word_agg_df
+
+
+def sent_feats(df):
+    df['sent'] = df['essay'].apply(lambda x: re.split('\\.|\\?|\\!',x))
+    df = df.explode('sent')
+    df['sent'] = df['sent'].apply(lambda x: x.replace('\n','').strip())
+    # Number of characters in sentences
+    df['sent_len'] = df['sent'].apply(lambda x: len(x))
+    # Number of words in sentences
+    df['sent_word_count'] = df['sent'].apply(lambda x: len(x.split(' ')))
+    df = df[df.sent_len!=0].reset_index(drop=True)
+
+    sent_agg_df = pd.concat([df[['id','sent_len']].groupby(['id']).agg(AGGREGATIONS), 
+                             df[['id','sent_word_count']].groupby(['id']).agg(AGGREGATIONS)], axis=1)
+    sent_agg_df.columns = ['_'.join(x) for x in sent_agg_df.columns]
+    sent_agg_df['id'] = sent_agg_df.index
+    sent_agg_df = sent_agg_df.reset_index(drop=True)
+    sent_agg_df.drop(columns=["sent_word_count_count"], inplace=True)
+    sent_agg_df = sent_agg_df.rename(columns={"sent_len_count":"sent_count"})
+    return sent_agg_df
+
+
+def parag_feats(df):
+    df['paragraph'] = df['essay'].apply(lambda x: x.split('\n'))
+    df = df.explode('paragraph')
+    # Number of characters in paragraphs
+    df['paragraph_len'] = df['paragraph'].apply(lambda x: len(x)) 
+    # Number of words in paragraphs
+    df['paragraph_word_count'] = df['paragraph'].apply(lambda x: len(x.split(' ')))
+    df = df[df.paragraph_len!=0].reset_index(drop=True)
+    
+    paragraph_agg_df = pd.concat([df[['id','paragraph_len']].groupby(['id']).agg(AGGREGATIONS), 
+                                  df[['id','paragraph_word_count']].groupby(['id']).agg(AGGREGATIONS)], axis=1) 
+    paragraph_agg_df.columns = ['_'.join(x) for x in paragraph_agg_df.columns]
+    paragraph_agg_df['id'] = paragraph_agg_df.index
+    paragraph_agg_df = paragraph_agg_df.reset_index(drop=True)
+    paragraph_agg_df.drop(columns=["paragraph_word_count_count"], inplace=True)
+    paragraph_agg_df = paragraph_agg_df.rename(columns={"paragraph_len_count":"paragraph_count"})
+    return paragraph_agg_df
+
+def product_to_keys(logs, essays):
+    essays['product_len'] = essays.essay.str.len()
+    tmp_df = logs[logs.activity.isin(['Input', 'Remove/Cut'])].groupby(['id']).agg({'activity': 'count'}).reset_index().rename(columns={'activity': 'keys_pressed'})
+    essays = essays.merge(tmp_df, on='id', how='left')
+    essays['product_to_keys'] = essays['product_len'] / essays['keys_pressed']
+    return essays[['id', 'product_to_keys']]
+
+def get_keys_pressed_per_second(logs):
+    temp_df = logs[logs['activity'].isin(['Input', 'Remove/Cut'])].groupby(['id']).agg(keys_pressed=('event_id', 'count')).reset_index()
+    temp_df_2 = logs.groupby(['id']).agg(min_down_time=('down_time', 'min'), max_up_time=('up_time', 'max')).reset_index()
+    temp_df = temp_df.merge(temp_df_2, on='id', how='left')
+    temp_df['keys_per_second'] = temp_df['keys_pressed'] / ((temp_df['max_up_time'] - temp_df['min_down_time']) / 1000)
+    return temp_df[['id', 'keys_per_second']]
